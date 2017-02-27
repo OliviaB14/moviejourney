2ff2531480956b2e65205626d89fbf1f7a1c9fa4
Maud <mdchlz123@gmail.com>
2017-02-27 09:25:51 +0100

    ajout base.php
    pensez à mettre vos codes dedans et après on ignore le fichier

---
+++ base.php
@@ -0,0 +1,19 @@
+<?php
+  // URL of the host
+  $dbhost = "localhost"; 
+  
+  // Name of the database
+  $dbname = "movie_journey_bdd";
+  
+  // User name
+  $dbuser = "";
+  
+  // Password (not used here)
+  $dbpass = "";
+ 
+  try {
+    $connection = new PDO('mysql:host='.$dbhost.';dbname='.$dbname, $dbuser, $dbpass);
+  } catch(PDOException $e) {
+    echo $e->getMessage();
+  }
+?>
