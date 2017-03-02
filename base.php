a25843674d4daf13b9e512d8c8337fda8fc250af
olivia <olivia.boumokonia9@etu.univ-lorraine.fr>
2017-03-02 08:50:08 +0100

    ignore base php pour de bon

--- base.php
+++ base.php
@@ -6,11 +6,9 @@
   $dbname = "movie_journey_bdd";
   
   // User name
-<<<<<<< HEAD
-  $dbuser = "ascgukp";
-=======
+
   $dbuser = "root";
->>>>>>> origin/master
+
   
   // Password (not used here)
   $dbpass = "qwsefvgy7uk";
