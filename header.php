b548b59687322586ca709fb7dec4fa52baf7000c
olivia <olivia.boumokonia9@etu.univ-lorraine.fr>
2017-03-07 18:56:29 +0100

    correction du logo flou
    via inkscape

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 			<span class="icon-bar"></span>
 			<span class="icon-bar"></span>
 		  </button>
-		  <a class="navbar-brand" href="index.php"><img src="image/logo-movie-journey.gif" id="logo" class="img-responsive"/></a>
+		  <a class="navbar-brand" href="index.php"><img src="image/logo-movie-journey.png" id="logo" class="img-responsive"/></a>
 		</div>
 	
 		<!-- Collect the nav links, forms, and other content for toggling -->
