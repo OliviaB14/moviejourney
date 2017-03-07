f80c73f2fb8a13083ba202865fc4f797db5890de
TheHishem <hishem.moueddene@yahoo.fr>
2017-03-07 18:09:37 +0100

    Circuit/php et js

---
+++ circuit.php
@@ -0,0 +1,36 @@
+<?php
+  // Connect to the database
+  require 'base.php';
+
+  // Character encoding of the database
+  $connection->exec("SET NAMES 'utf8'");
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Circuits</title>
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<?php
+	include('header.php');
+?>
+
+<div id="circuit1"> 
+	Circuit français :
+	<div id="map-canvas"></div>
+	
+
+
+<!-- Lien pour Maps --> <script type="text/javascript" src="http://maps.google.com/maps/api/js"></script>
+<!-- jquery script --> <script type="text/javascript" src="js/jquery-3.1.1.js"></script> 
+<!-- bootstrap file --> <script src="js/bootstrap.min.js"></script>
+<!-- Lien pour Maps --> <script src="js/maps.js"></script>
+
+
+</body>
+</html>
---
+++ js/maps.js
@@ -0,0 +1,44 @@
+// Google Map Api
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+var map;
+var nancy = new google.maps.LatLng(48.6893344, 6.1883330);
+var obj;
+//1 : 48.8562697, 2.2871275999999625
+//6 : 43.20667, 2.36361
+//7 : 44.840278, 1.145278
+var from = nancy;
+var to = new google.maps.LatLng(48.8562697, 2.2871275);
+// Event listener to initialize the map once the window has been loaded
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+function initialize() { 
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    var mapOptions = {
+        zoom: 12,
+        center: nancy
+    }
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    directionsDisplay.setMap(map);
+
+function processPath() {
+    // Build the request
+    var request = {
+        origin: from,
+        destination: to,
+        // Note that Javascript allows us to access the constant
+        // using square brackets and a string value as its
+        // "property."
+        travelMode: google.maps.TravelMode["DRIVING"]
+    };
+
+    // Send it
+    directionsService.route(request, function(response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            // Display the path on the map
+            directionsDisplay.setDirections(response);
+        }
+    });
+}
+}
