7f99761a6070e393489edb319ba8fcc4fd229bda
socosta <sophie9796@hotmail.fr>
2017-03-30 11:06:04 +0200

    Début de la page de cgv

---
+++ cgv.php
@@ -0,0 +1,70 @@
+<?php
+	require ('base.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+		<meta charset="utf-8"><!-- encoding -->
+		<meta name="description" content="Marchez sur les pas de vos acteurs préférés et visitez les lieux cultes de vos films préférés." /><!-- page description -->
+
+		<title>Conditions générales de ventes</title> <!-- page title -->
+
+		<!-- stylesheets files -->
+		<link href="css/style.css" rel="stylesheet" type="text/css"><!-- general stylesheet -->
+		<link href="css/bootstrap.css" rel="stylesheet" type="text/css"><!-- bootstrap -->
+		<link rel="stylesheet" type="text/css" href="css/qui-sommes-nous.css">
+
+		<link href="https://fonts.googleapis.com/css?family=Courgette" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Amaranth" rel="stylesheet">		
+		<!--font-family title -->
+</head>
+<body>
+	<?php
+		include ('header.php');
+	?>
+	<div class="jumbotron">
+		<div class="container">
+			<h1>Les conditions générales de ventes</h1>
+		</div>
+	</div>
+		<div class="container-fluid text-justify">
+			<div class="col-lg-8 col-lg-offset-2 col-xs-12">
+				<h3>Les informations suivantes sont en complément des conditons générales de voyage de la Commission de Litiges Voyages. Les conditions générales de voyage de la Commission de Litiges Voyages ainsi que les conditions générales de Movie Journey forment les conditions contractuelles applicables.</h3>
+
+				<h4>1.Inscritpion du Movie Journey</h4>
+				<p>1.1. L'inscritption à Movie Journey implique que l'utilisateur a pris connaissance des conditions générales du site et qu'il les accepte.</p>
+				<p>1.2. L'utilisateur ne peut pas s'inscrire plusieurs fois. </p>
+				<p>1.3. L'utilisateur est responsable des informations renseignées</p>
+				<p>1.4. Si l'utilisateur a accepté de recevoie les e-mails de Movie Journey lors de son insciption alors l'utilisateur est susceptible de revecoir des informations occasionnelles sur nos produits. Le consentement peut être révoqué à tout moment par notification écrite ou pas courriel au site. </p>
+
+				<h4>2. Confidentialité</h4>
+				<p>2.1. L'utilisateur est responsable de la confidentialité des informations de son compte. L'utilisateur ne doit jamais communiquer ses données personnelles, surtout pas son mot de passe.</p>
+				<p>2.2. En cas d'utilisation non autorisée des services Movie Journey part toute personne non associée au compte utilisé, l'utilisateur de ce compte en quesion doit immédiatemment informer Movie Journey.</p>
+				<p>2.3. La protection des données de l'utilisateur est la priorité de Movie Journey</p>
+
+				<h4>3. Prix</h4>
+				<p>3.1. Les prix du voyage peut dépendre de la formule choisie par l'utilisateur (transport, hébergement)</p>
+				<p>3.2. Nos prix sont calculés sur les tarifs, les redevances, les taux de changes et les taxes. </p>
+				<p>3.3. Nos prix n'incluent pas les suppléments de l'hébergement choisi, les excursions et les visites facultatives, les frais éventuels de visas, les vaccinations et autres formalités, les dépenses personnelles.  </p>
+
+				<h4>4. Conditions de voyages</h4>
+				<p>4.1. Tout voyageur doit s'assurer que passeports et visas (si nécessaire) sont en ordres.</p>
+				<p>4.2. Le voyageur doit s'assurer d'être en possession des titres de voyage avant le départ. Movie Journey décline toute responsabilité à l'égard des voyageurs qui se voient refuser l'accés à bord du transport choisi lors de la réservation ou dans un pays car ils ne sont pas en possession d'un document de voyage exigé par la compagnie de transport, l'autorité ou le pays, y compris les pays que le voyageur traverse pour se rendre à destination.</p>
+				<p>4.3. Le voyageur est tenu de consulter un médecin pour ce qui concerne les dernières recommandations en terme de vaccination et tout exigences en matière de santé.</p>
+
+
+				<h4>A</h4>
+			</div>	
+	</div>
+	<?php
+		include ('footer.php');
+	?>
+
+	<!-- SCRIPT FILES --> 
+	<!-- jquery script --><script type="text/javascript" src="js/jquery-3.1.1.js"></script> 
+	<!-- bootstrap file --> <script src="js/bootstrap.min.js"></script>
+	<!-- back to top button script --> <script type="text/javascript" src="js/backtotop.js"></script>
+
+
+</body>
+</html>
